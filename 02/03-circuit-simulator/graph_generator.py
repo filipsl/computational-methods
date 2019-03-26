fca2777cde0cf4075a92bc84e7df65893a1ff4fa
Filip Slazyk <filip.slazyk@gmail.com>
2019-03-26 14:19:41 +0100

    Generate graphs

--- 02/03-circuit-simulator/graph_generator.py
+++ 02/03-circuit-simulator/graph_generator.py
@@ -16,7 +16,6 @@ with open('./graphs_csv/random_graph.csv', 'w') as writeFile:
         writer.writerow(edge + (1,))
 writeFile.close()
 
-
 # 3-regular graph n=50 r = 1
 G = nx.random_regular_graph(3, 50)
 # nx.draw_spring(G, with_labels=True)
@@ -28,7 +27,6 @@ with open('./graphs_csv/3_regular_graph.csv', 'w') as writeFile:
         writer.writerow(edge + (1,))
 writeFile.close()
 
-
 # Graph with bridge n=40 r in (0,10)
 G = nx.erdos_renyi_graph(20, 0.2)
 edges = G.edges()
@@ -39,10 +37,9 @@ with open('./graphs_csv/graph_with_bridge.csv', 'w') as writeFile:
     G = nx.erdos_renyi_graph(20, 0.2)
     edges = G.edges()
     for edge in edges:
-        writer.writerow((edge[0] + 20,) + (edge[1] + 20,) + (np.random.rand()*10,))
-    writer.writerow((10, 30, np.random.rand()*10))  # add bridge
+        writer.writerow((edge[0] + 20,) + (edge[1] + 20,) + (np.random.rand() * 10,))
+    writer.writerow((10, 30, np.random.rand() * 10))  # add bridge
 writeFile.close()
-
 
 # 2D GRID r = 1
 G = nx.grid_graph(dim=[10, 10])
@@ -53,7 +50,6 @@ with open('./graphs_csv/2D_grid_const_r.csv', 'w') as writeFile:
         writer.writerow(tuple(map(lambda x: x[0] * 10 + x[1], edge)) + (1,))
 writeFile.close()
 
-
 # 2D GRID r in (0,10)
 G = nx.grid_graph(dim=[10, 10])
 edges = G.edges()
@@ -63,8 +59,6 @@ with open('./graphs_csv/2D_grid_var_r.csv', 'w') as writeFile:
         writer.writerow(tuple(map(lambda x: x[0] * 10 + x[1], edge))
                         + (np.random.rand() * 10,))
 writeFile.close()
-
-
 
 # G = nx.random_partition_graph([10, 10], 0.4, 0.1)
 #
